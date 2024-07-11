548fe56956fb176895d32003f8472546f79a2b70
TadeuGT <gabrieltadeugeromel@gmail.com>
2024-07-10 23:42:28 -0300

    Adicionado componente universal de botão
    NLW Aula 2

--- src/pages/create-trip/confirm-trip-modal.tsx
+++ src/pages/create-trip/confirm-trip-modal.tsx
@@ -1,5 +1,6 @@
 import { X, User, AtSign } from "lucide-react";
 import { FormEvent } from "react";
+import { Button } from "../../components/button";
 
 interface ConfirmTripModalProps {
     closeConfirmTripModal: () => void
@@ -45,9 +46,9 @@ export function ConfirmTripModal({
         />
         </div>
 
-        <button type="submit" className='w-full justify-center bg-lime-300 text-lime-950 rounded-lg px-5 h-11 font-medium flex items-center gap-2 hover:bg-lime-400'>
-        Confirmar criação da viagem
-        </button>
+        <Button type="submit" variant="primary" size="full" >
+            Confirmar criação da viagem
+        </Button>
 
     </form>
 
--- src/pages/trip-details/create-activity-modal.tsx
+++ src/pages/trip-details/create-activity-modal.tsx
@@ -1,4 +1,5 @@
 import { X, Tag, Calendar } from "lucide-react";
+import { Button } from "../../components/button";
 
 interface CreateActivityModalProps {
     closeCreateActivityModal: () => void
@@ -43,9 +44,9 @@ export function CreateActivityModal({
             </div>
 
             
-            <button type="submit" className='w-full justify-center bg-lime-300 text-lime-950 rounded-lg px-5 h-11 font-medium flex items-center gap-2 hover:bg-lime-400'>
-            Salvar atividade
-            </button>
+            <Button variant="primary" size="full">
+                Salvar atividade
+            </Button>
 
         </form>
 
